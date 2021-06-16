7a15cced4ecc3132d29dbba7c86f28a42c6b83b6
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-06-16 18:45:45 +0200

    Initial commit

---
+++ mediscreen/src/test/java/com/abernathyclinic/mediscreen/MediscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.mediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
