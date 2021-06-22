9bc11d9832e336aebdf97587ea3807fa2b44cf10
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-06-22 15:18:20 +0200

    Delete thymeleaf, refactor controller

--- mediscreen/src/main/java/com/abernathyclinic/mediscreen/controller/PatientController.java
+++ mediscreen/src/main/java/com/abernathyclinic/mediscreen/controller/PatientController.java
@@ -2,18 +2,11 @@ package com.abernathyclinic.mediscreen.controller;
 
 import javax.validation.Valid;
 
-import java.util.Iterator;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,38 +25,26 @@ public class PatientController {
 	private static Logger log = LoggerFactory.getLogger(PatientController.class);
 	
 	private IPatientService patientService;
-	
-	private final String TEMPLATE_ADD_PATIENT = "addpatient";
-	private final String TEMPLATE_LIST_PATIENT = "listpatient";
-	
+
 	@Autowired
 	public PatientController(IPatientService p_patientService)
 	{
 		patientService = p_patientService;
 	}
 	
-	//TODO Manage http status error code
 	@PostMapping(value = "patient/add")
-	public String addPatient(@Valid Patient patient, BindingResult result, Model model) {
+	public ResponseEntity<Patient> addPatient(@Valid Patient patient) {
 		
 		
     	log.info("POST Request to /patient/add with value: {}", patient);
     	
-    	if(result.hasErrors()) {
-    		log.warn("POST Request to /patient/add return errors, data: {}", patient);
-    		return TEMPLATE_ADD_PATIENT;
-    	}
-    	else {
-    		try {
-				patientService.create(patient);
-			} catch (PatientAlreadyExistsException e) {
-				log.warn("POST Request to /patient/add return error: {}", e.getMessage());
-				result.addError(new FieldError("lastName", "lastName", e.getMessage()));
-				return TEMPLATE_ADD_PATIENT;
-			}
-    	}
-        
-    	return "redirect:" + TEMPLATE_LIST_PATIENT;
+		try {
+			return new ResponseEntity<Patient>(patientService.create(patient), HttpStatus.CREATED);
+		} catch (PatientAlreadyExistsException e) {
+			log.warn("POST Request to /patient/add return error: {}", e.getMessage());
+			return new ResponseEntity<Patient>(HttpStatus.BAD_REQUEST);
+		}
+    	
 	}
 	
 	@ApiOperation(value = "Get the patient list")
