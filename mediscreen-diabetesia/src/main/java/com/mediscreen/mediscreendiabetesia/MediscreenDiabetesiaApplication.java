ac67ea57b3f6b0b9d07d25473b143b5847a8667a
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-07-03 15:19:50 +0200

    DiabeteIA initial commit

---
+++ mediscreen-diabetesia/src/main/java/com/mediscreen/mediscreendiabetesia/MediscreenDiabetesiaApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreendiabetesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenDiabetesiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenDiabetesiaApplication.class, args);
+	}
+
+}
