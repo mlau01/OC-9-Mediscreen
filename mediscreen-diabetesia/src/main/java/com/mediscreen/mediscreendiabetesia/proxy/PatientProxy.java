7cda62b4379a3b9862b936852c87d1f694d00c04
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-07-05 16:08:28 +0200

    Fix integration test add patients

--- mediscreen-diabetesia/src/main/java/com/mediscreen/mediscreendiabetesia/proxy/PatientProxy.java
+++ mediscreen-diabetesia/src/main/java/com/mediscreen/mediscreendiabetesia/proxy/PatientProxy.java
@@ -4,12 +4,7 @@ import feign.Param;
 import feign.RequestLine;
 
 public interface PatientProxy {
-
-	
 	@RequestLine("GET /patients/{patientId}")
 	Patient getPatient(@Param("patientId") int patientId);
-
-	@RequestLine("POST /patients/{patient}")
-	Patient addPatient(@Param("patient") String string);
 
 }
--- mediscreen-diabetesia/src/test/java/com/mediscreen/mediscreendiabetesia/DiabetesServiceIT.java
+++ mediscreen-diabetesia/src/test/java/com/mediscreen/mediscreendiabetesia/DiabetesServiceIT.java
@@ -1,56 +1,73 @@
 package com.mediscreen.mediscreendiabetesia;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mediscreen.mediscreendiabetesia.proxy.Patient;
-import com.mediscreen.mediscreendiabetesia.proxy.PatientProxy;
 import com.mediscreen.mediscreendiabetesia.service.DiabetesService;
 
 import feign.Feign;
+import feign.Headers;
+import feign.RequestLine;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @SpringBootTest
 public class DiabetesServiceIT {
+	
+	@AllArgsConstructor
+	@Data
+	class InnerPatient {
+		 private String firstName;
+		 private String lastName;
+		 private String dateOfBirth;
+		 private String sex;
+		 private String phone;
+		 private String address;
+	}
+	
+	interface InnerPatientProxy {
+		@RequestLine("POST /patients")
+		@Headers("Content-Type: application/json")
+		InnerPatient addPatient(InnerPatient patient);
+	}
 	
 	@Autowired
 	private DiabetesService diabetesService;
 	
 	@Test
-	@Disabled
+	//@Disabled
 	public void addDemoPatients(@Value("${patientapi.socket}") String patientApiSocket, @Autowired ObjectMapper objectMapper){
 		
-		List<Patient> patientList = new ArrayList<Patient>();
-		patientList.add(new Patient(null, "Lucas", "Fergusen", LocalDate.of(1968, 6, 22), "M", "387-866-1399", "2 Warren Street", null));
-		patientList.add(new Patient(null, "Pippa", "Rees", LocalDate.of(1952, 9, 27), "F", "628-423-0993", "745 West Valley Farms Drive", null));
-		patientList.add(new Patient(null, "Edward", "Arnold", LocalDate.of(1952, 11, 11), "M", "123-727-2779", "599 East Garden Ave", null));
-		patientList.add(new Patient(null, "Anthony", "Sharp", LocalDate.of(1946, 11, 26), "M", "451-761-8383", "894 Hall Street", null));
-		patientList.add(new Patient(null, "Wendy", "Ince", LocalDate.of(1958, 6, 29), "F", "802-911-9975", "4 Southampton Road", null));
-		patientList.add(new Patient(null, "Tracey", "Ross", LocalDate.of(1949, 12, 7), "F", "131-396-5049", "40 Sulphur Springs Dr", null));
-		patientList.add(new Patient(null, "Claire", "Wilson", LocalDate.of(1949, 12, 7), "F", "131-396-5049", "40 Sulphur Springs Dr", null));
-		patientList.add(new Patient(null, "Max", "Buckland", LocalDate.of(1945, 6, 24), "M", "833-534-0864", "193 Vale St", null));
-		patientList.add(new Patient(null, "Natalie", "Clark", LocalDate.of(1964, 6, 18), "F", "241-467-9197", "12 Beechwood Road", null));
-		patientList.add(new Patient(null, "Piers", "Bailey", LocalDate.of(1959, 6, 28), "M", "747-815-0557", "1202 Bumble Dr ", null));
+		List<InnerPatient> patientList = new ArrayList<InnerPatient>();
+		patientList.add(new InnerPatient("Lucas", "Fergusen", "1968-06-22", "M", "387-866-1399", "2 Warren Street"));
+		patientList.add(new InnerPatient("Pippa", "Rees", "1952-09-27", "F", "628-423-0993", "745 West Valley Farms Drive"));
+		patientList.add(new InnerPatient("Edward", "Arnold", "1952-11-11", "M", "123-727-2779", "599 East Garden Ave"));
+		patientList.add(new InnerPatient("Anthony", "Sharp", "1946-11-26", "M", "451-761-8383", "894 Hall Street"));
+		patientList.add(new InnerPatient("Wendy", "Ince", "1958-06-29", "F", "802-911-9975", "4 Southampton Road"));
+		patientList.add(new InnerPatient("Tracey", "Ross", "1949-12-07", "F", "131-396-5049", "40 Sulphur Springs Dr"));
+		patientList.add(new InnerPatient("Claire", "Wilson", "1949-12-07", "F", "131-396-5049", "40 Sulphur Springs Dr"));
+		patientList.add(new InnerPatient("Max", "Buckland", "1945-06-24", "M", "833-534-0864", "193 Vale St"));
+		patientList.add(new InnerPatient("Natalie", "Clark", "1964-06-18", "F", "241-467-9197", "12 Beechwood Road"));
+		patientList.add(new InnerPatient("Piers", "Bailey", "1959-06-28", "M", "747-815-0557", "1202 Bumble Dr "));
 		
-		PatientProxy patientProxy = Feign.builder().encoder(new GsonEncoder()).target(PatientProxy.class, patientApiSocket);
+		InnerPatientProxy patientProxy = Feign.builder()
+				.decoder(new GsonDecoder())
+				.encoder(new GsonEncoder())
+				.target(InnerPatientProxy.class, patientApiSocket);
 		
 		patientList.forEach((p) -> { 
-			try {
-				patientProxy.addPatient(objectMapper.writeValueAsString(p));
-			} catch (JsonProcessingException e) {
-				e.printStackTrace();
-			}
+				patientProxy.addPatient(p);
+
 		});
+		
 	}
 	@Test
 	public void getDiabetesRiskTest_shouldReturnCorrectRiskLevel() {
