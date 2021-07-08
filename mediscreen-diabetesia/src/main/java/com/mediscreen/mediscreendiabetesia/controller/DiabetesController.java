7de5707d67db0adaf6f60e04f035083dcbe6fb5c
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-07-09 01:44:01 +0200

    Add CrossOrigin to permit request form Angular

--- mediscreen-diabetesia/src/main/java/com/mediscreen/mediscreendiabetesia/controller/DiabetesController.java
+++ mediscreen-diabetesia/src/main/java/com/mediscreen/mediscreendiabetesia/controller/DiabetesController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mediscreen.mediscreendiabetesia.dto.PatientAssessDto;
@@ -19,6 +19,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200") //Allow HTTP request from angular
 public class DiabetesController {
 	private static Logger logger = LoggerFactory.getLogger(DiabetesController.class);
 	private IDiabetesService diabetesService;
